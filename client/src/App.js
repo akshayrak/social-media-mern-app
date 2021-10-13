94f0e5547c9b713846585f68ef2c8f0fc074bbf5
Akshay R <akshayrak77@gmail.com>
2021-10-13 23:33:22 +0530

    react initial setup is done and need to start with css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from "@mui/material"
+import Posts from './components/posts/Posts';
+import Form from './components/form/Form';
 
 const App = () => {
+    const {REACT_APP_ICON_IMAGE}=process.env;
+
     return (
-        <div>
-            app
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Title of the App</Typography>
+                <img src={REACT_APP_ICON_IMAGE} alt="someImage" height="60" width="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+            
+        </Container>
     )
 }
 
---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './post/Post'
+
+const Posts = () => {
+    return (
+        <div>
+            <>
+                <Post />
+            </>
+
+        </div>
+    )
+}
+
+export default Posts

a0384e3587de384981da193de1703e0a0a1c5191
Akshay R <akshayrak77@gmail.com>
2021-10-16 22:58:11 +0530

    tried to copy paste the same css form git repo but still doesn't work

--- client/src/styles.js
+++ client/src/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@mui/styles';
 
 export default makeStyles(() => ({
   appBar: {
-    borderRadius: '15px',
+    borderRadius: 15,
     margin: '30px 0',
     display: 'flex',
     flexDirection: 'row',
